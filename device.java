05fdb98d6ccfb3531daa743803199db6fc106fe1
Jnoox <114774799+Jnoox@users.noreply.github.com>
2023-05-18 14:36:13 +0300

    Add files via upload

---
+++ device.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package donationproject;
+
+/**
+ *
+ * @author Jana
+ */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+class device {
+    private String name, status, type,description;
+    private int duration;
+    
+    public device(String name, String status, String type, int duration,String description){
+        this.name = name;
+        this.status = status;
+        this.type = type;
+        this.duration =duration;
+        this.description=description;
+    }
+    
+    public String deviceDetail(String name, String status, String type, int duration){
+        String Detail;
+        if(duration != 0){
+            Detail = "Device name: " +name+ " | " 
+                   + "Device type: " +type+ " | "
+                   + "Device status: " +status+ " | "
+                   + "Device usage time in years: " +duration+" | "
+                   + "Device description: "+description;
+        }else{
+            Detail = "Device name: " +name+ " | " 
+                   + "Device type: " +type+ " | "
+                   + "Device status: " +status+" | "
+              + "Device description: "+description;
+        }
+        return Detail;
+    }
+    
+    public void addDevice() throws IOException {
+        try{
+        FileWriter listDevice = new FileWriter("arwa.txt", true);
+        PrintWriter availableDevices = new PrintWriter(listDevice); 
+        
+        availableDevices.println(deviceDetail(name,status,type,duration));
+        
+        availableDevices.close(); //close file
+        }catch(Exception e){
+            
+        }
+    }
+}
